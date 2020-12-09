bc540a7fc12cb1131148a3f41704f83c81bb0d29
may arden <mayard3n@gmail.com>
2020-12-09 13:51:47 -0800

    commit tree 6d22b7149f7dee92540c3edc61e9fbae01c1ee8b

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@aships = []
+
+    def initialize(name, type, booty)
+        @name = name
+        @type = type
+        @booty = booty
+        @@ships << self
+    end 
+
+    def self.all
+        @@ships
+    end 
+
+    def self.clear
+        @@ships = [] 
+    end 
+
+
+
 end
